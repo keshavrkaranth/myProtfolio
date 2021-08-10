ae756517267bcd1d337525b70278aa056ee042e4
Keshavkaranth <keshav@ohlocal.in>
2021-08-10 23:41:56 +0530

    added new resume and AWS icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   // owl-carousel for projects
   $('.owl-carousel').owlCarousel({
-    loop:true,
+    loop: true,
     nav: true,
     dots: false,
     items: 1,
@@ -12,11 +12,11 @@ $(document).ready(function() {
   let selector = (e) => {
     return document.querySelector(e)
   }
-  
+
   // Nav bar toggle event
   selector('.side-menu-close').addEventListener('click', () => {
-  
-    if(selector('.side-menu-close').classList.contains('closed')){
+
+    if (selector('.side-menu-close').classList.contains('closed')) {
       selector('.side-nav-content').classList.remove('opened');
       selector('.side-menu-close').classList.remove('closed');
       selector('body').style = 'overflow: visible';
@@ -27,7 +27,7 @@ $(document).ready(function() {
     }
 
   })
-  
+
   $('.nav-link').on('click', () => {
     selector('.side-nav-content').classList.remove('opened');
     selector('.side-menu-close').classList.remove('closed');
@@ -45,10 +45,10 @@ $(document).ready(function() {
   });
 
   // Reset nav bar on large size
-  window.addEventListener('resize', function(event){
+  window.addEventListener('resize', function (event) {
     var newWidth = window.innerWidth;
-    
-    if(newWidth > 991) {
+
+    if (newWidth > 991) {
       selector('.side-nav-content').classList.remove('opened');
       selector('.side-menu-close').classList.remove('closed');
       selector('body').style = 'overflow: visible';
@@ -64,33 +64,33 @@ $(document).ready(function() {
     var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
     let name = $('#name').val().trim(),
-    email = $('#email').val().trim(),
-    message = $('#message').val().trim();
+      email = $('#email').val().trim(),
+      message = $('#message').val().trim();
 
-    if(name == '') {
+    if (name == '') {
       $("#name").after('<span class="err">Please enter your name.</span>');
       hasError = true;
     }
 
-    if(email == '') {
+    if (email == '') {
       $("#email").after('<span class="err">Please enter your email address.</span>');
       hasError = true;
     }
 
-    if(!emailReg.test(email)) {
+    if (!emailReg.test(email)) {
       $("#email").after('<span class="err">Enter a valid email address.</span>');
       hasError = true;
     }
 
-    if(message == '') {
+    if (message == '') {
       $("#message").after('<span class="err">Please enter your message.</span>');
       hasError = true;
     }
 
     e.preventDefault()
 
-    if(hasError == true) { 
-      return false; 
+    if (hasError == true) {
+      return false;
     } else {
       $("#name").val("");
       $("#email").val("");
